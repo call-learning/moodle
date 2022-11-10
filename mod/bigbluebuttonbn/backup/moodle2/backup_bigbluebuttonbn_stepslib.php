e27812d4a9653a0b01e8dfa0966785a00092da65
Laurent David <lmedavid@gmail.com>
2022-11-10 23:20:04 +0100

    MDL-74638 mod_bigbluebuttonbn: Do not backup guest credentials
    * Guest access URL and password should not be copied when duplicating or backing
    up the activity.

--- mod/bigbluebuttonbn/backup/moodle2/backup_bigbluebuttonbn_stepslib.php
+++ mod/bigbluebuttonbn/backup/moodle2/backup_bigbluebuttonbn_stepslib.php
@@ -52,7 +52,7 @@ class backup_bigbluebuttonbn_activity_structure_step extends backup_activity_str
             'clienttype', 'muteonstart', 'completionattendance',
             'completionengagementchats', 'completionengagementtalks', 'completionengagementraisehand',
             'completionengagementpollvotes', 'completionengagementemojis',
-            'guestallowed', 'mustapproveuser', 'guestlinkuid', 'guestpassword']);
+            'guestallowed', 'mustapproveuser']);
 
         $logs = new backup_nested_element('logs');
 
